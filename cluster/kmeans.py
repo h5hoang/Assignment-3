127d01b19e9f5d6c865c9e83963acfd869df0d59
h5hoang <hanson.hoang@ucsf.edu>
2024-11-20 16:58:27 -0800

    implementation + main

--- cluster/kmeans.py
+++ cluster/kmeans.py
@@ -46,40 +46,48 @@ class KMeans():
         
         # In the line below, you need to randomly select where the centroid's positions will be.
         # Also set your initialized centroid to be your random centroid position
+        random_indices = np.random.choice(matrix.shape[0], self.k, replace=False)
+        self.centroid = matrix[random_indices]
 
-        
         # In the line below, calculate the first distance between your randomly selected centroid positions
         # and the data points
-
+        distances = cdist(matrix, self.centroid, metric=self.metric)
         
         # In the lines below, Create a for loop to keep assigning data points to clusters, updating centroids, 
         # calculating distance and error until the iteration limit you set is reached
+        for iteration in range(self.max_iter):
 
             # Within the loop, find the each data point's closest centroid
-
-        
+            labels = np.argmin(distances, axis=1)
         
             # Within the loop, go through each centroid and update the position.
             # Essentially, you calculate the mean of all data points assigned to a specific cluster. This becomes the new position for the centroid
-
-                
+            new_centroids = []
             
+            for i in range (self.k):
+                cluster_points = matrix[labels ==i]
+                if len(cluster_points) > 0:
+                    new_centroid = cluster_points.mean(axis=0)
+                else:
+                    new_centroid = self.centroids[i]
+                new_centroids.append(new_centroid)
+            new_centroids = np.array(new_centroids)
             # Within the loop, calculate distance of data point to centroid then calculate MSE or SSE (inertia)
-
-        
+            distances = cdist(matrix, new_centroids, metric=self.metric)
+            SSE = (matrix - new_centroids[labels])**2
+            error = np.sum(SSE)
             
             # Within the loop, compare your previous error and the current error
             # Break if the error is less than the tolerance you've set 
-
+            if abs(self.error - error) < self.tol:
                 
                 # Set your error as calculated inertia here before you break!
+                self.error = error
+                break
 
-            
             # Set error as calculated inertia
-
-        
-            
-    
+            self.centroid = new_centroids
+            self.error = error 
     
     def predict(self, matrix: np.ndarray) -> np.ndarray:
         """
@@ -93,7 +101,8 @@ class KMeans():
                 An array/list of predictions will be returned.
         """
         # In the line below, return data point's assignment 
-        pass
+        distances = cdist(matrix, self.centroid, metric=self.metric)
+        return np.argmin(distances, axis=1)
     
     def get_error(self) -> float:
         """
@@ -104,7 +113,7 @@ class KMeans():
                 inertia of your fit
 
         """
-        pass
+        return self.error
     
     
     def get_centroids(self) -> np.ndarray:
@@ -113,6 +122,6 @@ class KMeans():
         Your centroid positions will be returned. 
         """
         # In the line below, return centroid location
-        pass
+        return self.centroid
         
     
--- main.py
+++ main.py
@@ -14,29 +14,39 @@ def main():
     og_iris = np.array(load_iris().data)
     
     # Initialize your KMeans algorithm
-    
+    kmeans = KMeans(k=5, max_iter=1000, tol=0.000001, metric='euclidean')
     
     # Fit model
-    
+    kmeans.fit(og_iris) 
 
     # Load new dataset
     df = np.array(pd.read_csv('data/iris_extended.csv', 
                 usecols = ['petal_length', 'petal_width', 'sepal_length', 'sepal_width']))
 
     # Predict based on this new dataset
-    
+    prediction = kmeans.predict(df)  
     
     # You can choose which scoring method you'd like to use here:
-    
+    final_error = kmeans.get_error()
+    print("Final error (inertia):", final_error)   
     
     # Plot your data using plot_3d_clusters in visualization.py
-
+    plot_3d_clusters(df, prediction, kmeans.get_centroids(), final_error)
     
     # Try different numbers of clusters
-
+    k_values = range(1, 10)
+    errors = []
+    for k in k_values:
+        kmeans = KMeans(k=k, max_iter=300, tol=1e-4, metric='euclidean')
+        kmeans.fit(df)
+        errors.append(kmeans.get_error())
     
     # Plot the elbow plot
-
+    plt.plot(k_values, errors, marker='o')
+    plt.title('Elbow Plot')
+    plt.xlabel('# of Clusters, k')
+    plt.ylabel('Inertia (Error)')
+    plt.show()
     
     # Question: 
     # Please answer in the following docstring how many species of flowers (K) you think there are.
@@ -44,12 +54,8 @@ def main():
     
     """
     How many species of flowers are there: 
-    
+
     Reasoning: 
-    
-    
-    
-    
     """
 
     
